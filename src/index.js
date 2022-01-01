267deaed3b52a0d74c702f42bc7e5b13cd52b6e3
Your Name <you@example.com>
2022-01-01 18:52:10 +0300

    add babel to project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-const elvenShielRecipe = {
+const elvenShieldRecipe = {
     leatherStripe: 2,
     ironIngot: 1,
     refineMoonstone: 4,
 };
 
-console.log(elvenShielRecipe);
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 2,
+    refineMoonstone: 4,
+};
+
+console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
